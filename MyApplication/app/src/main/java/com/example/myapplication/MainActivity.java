68b02931b41088135c79202ba37fe00f707a0fef
Simona Prokic <167simonap@gmail.com>
2020-04-28 21:44:55 +0200

    Add settings

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,28 +2,16 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
 import com.example.myapplication.ui.GroupActivitiesActivity;
 import com.example.myapplication.ui.HomeActivity;
-import com.example.myapplication.ui.LoginActivity;
 import com.example.myapplication.ui.ProfileActivity;
 import com.example.myapplication.ui.TrackingActivity;
-import com.example.myapplication.ui.fragments.AccessibilityFragment;
-import com.example.myapplication.ui.fragments.ActivityFragment;
-import com.example.myapplication.ui.fragments.HomeFragment;
-import com.example.myapplication.ui.fragments.ProfileFragment;
-import com.example.myapplication.util.SaveSharedPreference;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -69,8 +57,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
 
 }
--- MyApplication/app/src/main/java/com/example/myapplication/ui/ProfileActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/ProfileActivity.java
@@ -1,9 +1,12 @@
 package com.example.myapplication.ui;
 
-import android.content.ClipData;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.myapplication.ui.fragments.NotificationsSettingsFragment;
+import com.example.myapplication.ui.fragments.PrivacySettingsFragment;
+import com.example.myapplication.ui.fragments.ThemeSettingsFragment;
 import com.example.myapplication.util.SaveSharedPreference;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,39 +14,57 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.example.myapplication.R;
 import com.google.android.material.navigation.NavigationView;
 
-public class ProfileActivity extends AppCompatActivity {
-
+public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     BottomNavigationView bottom_navigation;
     Intent intent;
     DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Menu menu;
     Toolbar toolbar;
-//    NavigationView navigationView;
-    ActionBarDrawerToggle toggle;
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
+        setToolbar();
+        setDrawerLayout(this);
+        setNavigationView();
+        setBottomNavigation();
+    }
+
+    private void setToolbar() {
         toolbar = findViewById(R.id.toolbar);
-        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawerOpen, R.string.drawerClose);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    private void setDrawerLayout(ProfileActivity thisProfileActivity) {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(thisProfileActivity, drawerLayout, toolbar, R.string.drawerOpen, R.string.drawerClose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+    }
 
+    private void setNavigationView() {
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setBottomNavigation() {
         bottom_navigation = findViewById(R.id.bottom_navigation);
-        Menu menu = bottom_navigation.getMenu();
+        menu = bottom_navigation.getMenu();
         MenuItem menuItem = menu.getItem(2);
         menuItem.setChecked(true);
         bottom_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -72,27 +93,53 @@ public class ProfileActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-        Button logoutBT = findViewById(R.id.logoutBT);
-        logoutBT.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Set LoggedIn status to false
-                SaveSharedPreference.setLoggedIn(ProfileActivity.this, false);
-                // Logout
-                logout();
-
-            }
-        });
-
     }
 
-
-    public void logout() {
-        Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
-        startActivity(intent);
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.theme:
+                adaptNavigationDrawer();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ThemeSettingsFragment()).commit();
+                break;
+            case R.id.notifications:
+                adaptNavigationDrawer();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new NotificationsSettingsFragment()).commit();
+                break;
+            case R.id.privacy:
+                adaptNavigationDrawer();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PrivacySettingsFragment()).commit();
+                break;
+            case R.id.logout:
+                SaveSharedPreference.setLoggedIn(this, false);
+                intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.profile_option:
+                navigationView.getMenu().findItem(R.id.profile_option).setVisible(false);
+                intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
+    private void adaptNavigationDrawer() {
+        menu.clear();
+        navigationView.getMenu().findItem(R.id.profile_option).setVisible(true);
+    }
 
-
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
