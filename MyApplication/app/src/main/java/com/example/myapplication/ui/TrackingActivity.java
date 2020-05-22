15fed98fffd3120fa0f85032891c907244b7ca63
Simona Prokic <167simonap@gmail.com>
2020-05-22 19:58:10 +0200

    Track activity

--- MyApplication/app/src/main/java/com/example/myapplication/ui/TrackingActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/TrackingActivity.java
@@ -2,20 +2,22 @@ package com.example.myapplication.ui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDialog;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -33,68 +35,69 @@ import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 
-public class TrackingActivity extends AppCompatActivity implements LocationListener {
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
-    BottomNavigationView bottom_navigation;
-    Intent intent;
-    Button startButton;
-    Button stopButton;
-    MapView openMapView;
-    MapController mapController;
-    protected LocationManager locationManager;
-    GeoPoint currentLocation;
-    Marker marker;
+public class TrackingActivity extends AppCompatActivity implements LocationListener, SensorEventListener {
+
+    private Intent intent;
+    private BottomNavigationView bottomNavigationView;
+    private MapView mapView;
+    private Button startButton;
+    private Button stopButton;
+    private TextView stepsTextView;
+    private TextView durationTextView;
+    private TextView distanceTextView;
+    private LocationManager locationManager;
+    private MapController mapController;
+    private GeoPoint currentLocation;
+    private Marker currentLocationMarker;
+    private Marker trackingLocationMarker;
+    private static Marker startMarker;
+    private static Marker endMarker;
+    boolean trackingStarted = false;
+    private float initialSteps = -1;
+    private float calculatedSteps;
+    private static float finalSteps;
+    private Timer timer = new Timer();
+    private long durationInSeconds;
+    private static long finalDuration;
+    private double distance;
+    private static double finalDistance;
+    private int userHeight = 170;
+    private double userStepLength = userHeight *  0.413;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
 
-        setMapPermission();
-
+        findAllViews();
         setBottomNavigation();
-        configureStopButton();
+        setMapPermission();
         configureMap();
+        configureStartButton();
+        configureStopButton();
     }
 
-    private void setMapPermission() {
-        Configuration.getInstance().setUserAgentValue(getPackageName());
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                //
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-
-            }
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                //
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-
-            }
-        }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (location == null) {
-            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            if (location != null) {
-                currentLocation = new GeoPoint(location.getLatitude(), location.getLongitude());
-            }
-        } else {
-            currentLocation = new GeoPoint(location.getLatitude(), location.getLongitude());
-        }
+    private void findAllViews() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        startButton = findViewById(R.id.start_button);
+        stopButton = findViewById(R.id.stop_button);
+        mapView = findViewById(R.id.tracking_map);
+        stepsTextView = findViewById(R.id.steps_text_view);
+        durationTextView = findViewById(R.id.duration_text_view);
+        distanceTextView = findViewById(R.id.distance_text_view);
     }
 
     private void setBottomNavigation() {
-        bottom_navigation = findViewById(R.id.bottom_navigation);
-        Menu menu = bottom_navigation.getMenu();
+        Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
-        bottom_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
@@ -118,34 +121,165 @@ public class TrackingActivity extends AppCompatActivity implements LocationListe
         });
     }
 
-    private void configureStopButton() {
-        stopButton = findViewById(R.id.stop_button);
-        stopButton.setOnClickListener(new View.OnClickListener() {
+    private void setMapPermission() {
+        Configuration.getInstance().setUserAgentValue(getPackageName());
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+        if (locationManager != null) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+    }
+
+    private void configureMap() {
+        mapView.getZoomController().activate();
+        mapController = (MapController) mapView.getController();
+        mapController.setZoom(18);
+        mapController.setCenter(currentLocation);
+
+        getLastKnownLocation();
+        initializeMarkers();
+        setCurrentLocationMarkerOnMap();
+    }
+
+    private void getLastKnownLocation() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location == null) {
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if (location != null) {
+                currentLocation = new GeoPoint(location.getLatitude(), location.getLongitude());
+            }
+        } else {
+            currentLocation = new GeoPoint(location.getLatitude(), location.getLongitude());
+        }
+    }
+
+    private void initializeMarkers() {
+        trackingLocationMarker = new Marker(mapView);
+        startMarker = new Marker(mapView);
+        endMarker = new Marker(mapView);
+        currentLocationMarker = new Marker(mapView);
+    }
+
+    private void setCurrentLocationMarkerOnMap() {
+        currentLocationMarker.setPosition(currentLocation);
+        currentLocationMarker.setIcon(this.getResources().getDrawable(R.drawable.ic_my_location_orange_24dp));
+        currentLocationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        mapView.getOverlays().add(currentLocationMarker);
+    }
+
+    private void configureStartButton() {
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openShareActivityDialog();
+                if (ActivityCompat.checkSelfPermission(TrackingActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(TrackingActivity.this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                trackingStarted = true;
+                startButton.setEnabled(false);
+                stopButton.setEnabled(true);
+                setTimer();
+                setStepCounterSensor();
+                removeMarkerFromMap(currentLocationMarker);
+                setTrackingLocationMarkerOnMap();
+                setStartMarker();
             }
         });
     }
 
-    private void configureStartButton() {
-        startButton = findViewById(R.id.start_button);
-        startButton.setOnClickListener(new View.OnClickListener() {
+    private void setTimer() {
+        durationInSeconds = 0;
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        durationTextView.setText(formatTime(durationInSeconds));
+                        durationInSeconds++;
+                    }
+                });
+            }
+        }, 0, 1000);
+    }
+
+    @SuppressLint("DefaultLocale")
+    private String formatTime(long durationInSeconds) {
+        long hours = durationInSeconds / 3600;
+        long minutes = (durationInSeconds % 3600) / 60;
+        long seconds = durationInSeconds % 60;
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    private void setStepCounterSensor() {
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor;
+        if (sensorManager != null) {
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    private void removeMarkerFromMap(Marker marker) {
+        mapView.getOverlays().remove(marker);
+    }
+
+    private void setStartMarker() {
+        startMarker.setPosition(currentLocation);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker.setIcon(this.getResources().getDrawable(R.drawable.ic_location_on_green_24dp));
+    }
+
+    private void configureStopButton() {
+        stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (ActivityCompat.checkSelfPermission(TrackingActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(TrackingActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                currentLocation = new GeoPoint(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+                finalSteps = calculatedSteps;
+                initialSteps = -1;
+                finalDistance = distance;
+                distance = 0;
+                finalDuration = durationInSeconds;
+                durationInSeconds = 0;
+                startButton.setEnabled(true);
+                stopButton.setEnabled(false);
+                resetTrackingInfo();
+                openShareActivityDialog();
+                resetTimer();
             }
         });
+    }
+
+    private void resetTimer() {
+        timer.cancel();
+        timer = new Timer();
+    }
+
+    private void resetTrackingInfo() {
+        trackingStarted = false;
+        durationTextView.setText("0");
+        stepsTextView.setText("0");
+        distanceTextView.setText("0");
+        removeMarkerFromMap(trackingLocationMarker);
     }
 
     private void openShareActivityDialog() {
@@ -153,45 +287,78 @@ public class TrackingActivity extends AppCompatActivity implements LocationListe
         shareActivityDialog.show(getSupportFragmentManager(), "Share dialog");
     }
 
-    private void configureMap() {
-        openMapView = findViewById(R.id.tracking_map);
-        openMapView.getZoomController().activate();
+    public static Marker getStartMarker() {
+        return startMarker;
+    }
 
-        mapController = (MapController) openMapView.getController();
-        mapController.setZoom(18);
-        mapController.setCenter(currentLocation);
-
-        marker = new Marker(openMapView);
-        marker.setPosition(currentLocation);
-        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        openMapView.getOverlays().add(marker);
+    public static Marker getEndMarker() {
+        return endMarker;
     }
 
     @Override
     public void onLocationChanged(Location location) {
-//        TextView txtLat = findViewById(R.id.textview1);
-//        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
-//        currentLocation = new GeoPoint(location.getLatitude(), location.getLongitude());
-//        mapController.setCenter(currentLocation);
+        currentLocation = new GeoPoint(location.getLatitude(), location.getLongitude());
+        mapController.setCenter(currentLocation);
+        if (trackingStarted) {
+            removeMarkerFromMap(trackingLocationMarker);
+            setTrackingLocationMarkerOnMap();
+            setEndMarker();
+        } else {
+            removeMarkerFromMap(currentLocationMarker);
+            setCurrentLocationMarkerOnMap();
+        }
+    }
 
-        openMapView.getOverlays().remove(marker);
-        marker.setPosition(currentLocation);
-        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        openMapView.getOverlays().add(marker);
+    private void setTrackingLocationMarkerOnMap() {
+        trackingLocationMarker.setPosition(currentLocation);
+        trackingLocationMarker.setIcon(this.getResources().getDrawable(R.drawable.ic_location_on_orange_24dp));
+        trackingLocationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        mapView.getOverlays().add(trackingLocationMarker);
+    }
+
+    private void setEndMarker() {
+        endMarker.setPosition(currentLocation);
+        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        endMarker.setIcon(this.getResources().getDrawable(R.drawable.ic_location_on_red_24dp));
     }
 
     @Override
-    public void onProviderDisabled(String provider) {
-        //
+    public void onSensorChanged(SensorEvent event) {
+        if (initialSteps == -1) {
+            initialSteps = event.values[0];
+            calculatedSteps = 0;
+        } else {
+            calculatedSteps = event.values[0] - initialSteps;
+            distance = (calculatedSteps * userStepLength) / 100000;
+        }
+        distanceTextView.setText(formatDistance(distance));
+        stepsTextView.setText(String.valueOf((int)calculatedSteps));
+    }
+
+    private String formatDistance(double distance) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+        return df.format(distance);
+    }
+
+    public static ArrayList getData() {
+        ArrayList data = new ArrayList();
+        data.add(finalDistance);
+        data.add(finalDuration);
+        data.add((int)finalSteps);
+        return data;
     }
 
     @Override
-    public void onProviderEnabled(String provider) {
-        //
-    }
+    public void onProviderDisabled(String provider) {}
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        //
-    }
+    public void onProviderEnabled(String provider) {}
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
 }
