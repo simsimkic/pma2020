461387a2989cb5c50c265f36dd15462de90a3a59
Dejan Predojevic <dejanpredojevic11@gmail.com>
2020-06-02 14:08:06 +0200

    back login and registration

---
+++ running/src/main/java/com/pma/running/controler/UserControler.java
@@ -0,0 +1,29 @@
+package com.pma.running.controler;
+
+import com.pma.running.dto.LoginRequest;
+import com.pma.running.model.User;
+import com.pma.running.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "", allowedHeaders = "", maxAge = 3600)
+@RequestMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserControler {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/registerUser")
+    public ResponseEntity<User> saveUser(@RequestBody User user) throws Exception{
+        return new ResponseEntity<User>(userService.save(user), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/loginUser", method = RequestMethod.POST)
+    public ResponseEntity<Boolean> login(@RequestBody LoginRequest loginDTO) throws Exception {
+        return new ResponseEntity<Boolean>(userService.login(loginDTO), HttpStatus.OK);
+    }
+}
