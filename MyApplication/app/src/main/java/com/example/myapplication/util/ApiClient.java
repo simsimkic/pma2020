83cb4046cfa93d759d9ea9aac656566536e71109
Dejan Predojevic <dejanpredojevic11@gmail.com>
2020-06-25 12:05:05 +0200

    merge

--- MyApplication/app/src/main/java/com/example/myapplication/util/ApiClient.java
+++ MyApplication/app/src/main/java/com/example/myapplication/util/ApiClient.java
@@ -5,16 +5,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public static final String BASE_URL = "http://192.168.1.5:8080/";
+    public static final String BASE_URL = "http://192.168.1.4:8080/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
+
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
+
         return retrofit;
     }
 
